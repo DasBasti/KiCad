2716da3899418a9a0076227ba6c2648b61c62ca9
Bastian Neumann <bastian.neumann@dentsplysirona.com>
2017-08-23 13:45:49 +0200

    Modified Dialog layout for via class generation

--- pcbnew/dialogs/dialog_via_layers_base.cpp
+++ pcbnew/dialogs/dialog_via_layers_base.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Feb 16 2016)
+// C++ code generated with wxFormBuilder (version Aug 23 2017)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO "NOT" EDIT THIS FILE!
@@ -17,6 +17,60 @@ DIALOG_VIA_LAYERS_BASE::DIALOG_VIA_LAYERS_BASE( wxWindow* parent, wxWindowID id,
 	
 	wxBoxSizer* bSizes;
 	bSizes = new wxBoxSizer( wxVERTICAL );
+	
+	wxBoxSizer* bSizer5;
+	bSizer5 = new wxBoxSizer( wxHORIZONTAL );
+	
+	wxBoxSizer* bSizer6;
+	bSizer6 = new wxBoxSizer( wxVERTICAL );
+	
+	m_gridViaStack = new wxGrid( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+	
+	// Grid
+	m_gridViaStack->CreateGrid( 10, 3 );
+	m_gridViaStack->EnableEditing( false );
+	m_gridViaStack->EnableGridLines( true );
+	m_gridViaStack->EnableDragGridSize( false );
+	m_gridViaStack->SetMargins( 0, 0 );
+	
+	// Columns
+	m_gridViaStack->AutoSizeColumns();
+	m_gridViaStack->EnableDragColMove( false );
+	m_gridViaStack->EnableDragColSize( false );
+	m_gridViaStack->SetColLabelSize( 30 );
+	m_gridViaStack->SetColLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
+	
+	// Rows
+	m_gridViaStack->EnableDragRowSize( true );
+	m_gridViaStack->SetRowLabelSize( 80 );
+	m_gridViaStack->SetRowLabelAlignment( wxALIGN_CENTRE, wxALIGN_CENTRE );
+	
+	// Label Appearance
+	
+	// Cell Defaults
+	m_gridViaStack->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_TOP );
+	m_gridViaStack->SetMinSize( wxSize( 320,-1 ) );
+	m_gridViaStack->SetMaxSize( wxSize( -1,200 ) );
+	
+	bSizer6->Add( m_gridViaStack, 0, wxALL|wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer7;
+	bSizer7 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_buttonAddVia = new wxButton( this, wxID_ANY, _("MyButton"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer7->Add( m_buttonAddVia, 0, wxALL, 5 );
+	
+	m_buttonRemoveVia = new wxButton( this, wxID_ANY, _("MyButton"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer7->Add( m_buttonRemoveVia, 0, wxALL, 5 );
+	
+	
+	bSizer6->Add( bSizer7, 0, wxALIGN_CENTER|wxBOTTOM|wxLEFT|wxRIGHT|wxTOP, 5 );
+	
+	
+	bSizer5->Add( bSizer6, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer2;
+	bSizer2 = new wxBoxSizer( wxVERTICAL );
 	
 	wxFlexGridSizer* fgSizer1;
 	fgSizer1 = new wxFlexGridSizer( 0, 3, 0, 0 );
@@ -36,9 +90,9 @@ DIALOG_VIA_LAYERS_BASE::DIALOG_VIA_LAYERS_BASE( wxWindow* parent, wxWindowID id,
 	m_staticText8->Wrap( -1 );
 	fgSizer1->Add( m_staticText8, 0, wxALL, 5 );
 	
-	m_staticText9 = new wxStaticText( this, wxID_ANY, _("Through"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_staticText9->Wrap( -1 );
-	fgSizer1->Add( m_staticText9, 0, wxALL, 5 );
+	m_staticTextViaName = new wxStaticText( this, wxID_ANY, _("Through"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticTextViaName->Wrap( -1 );
+	fgSizer1->Add( m_staticTextViaName, 0, wxALL, 5 );
 	
 	m_ViaStartLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
 	fgSizer1->Add( m_ViaStartLayer, 0, wxALL, 5 );
@@ -46,41 +100,17 @@ DIALOG_VIA_LAYERS_BASE::DIALOG_VIA_LAYERS_BASE( wxWindow* parent, wxWindowID id,
 	m_ViaEndLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
 	fgSizer1->Add( m_ViaEndLayer, 0, wxALL, 5 );
 	
-	m_staticText5 = new wxStaticText( this, wxID_ANY, _("Blind/Buried"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_staticText5->Wrap( -1 );
-	fgSizer1->Add( m_staticText5, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
 	
-	m_BlindBurriedViaStartLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	fgSizer1->Add( m_BlindBurriedViaStartLayer, 0, wxALL, 5 );
-	
-	m_BlindBurriedViaEndLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	fgSizer1->Add( m_BlindBurriedViaEndLayer, 0, wxALL, 5 );
-	
-	m_staticText7 = new wxStaticText( this, wxID_ANY, _("Micro Top"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_staticText7->Wrap( -1 );
-	fgSizer1->Add( m_staticText7, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
-	
-	m_MicroViaTopStartLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	fgSizer1->Add( m_MicroViaTopStartLayer, 0, wxALL, 5 );
-	
-	m_MicroViaTopEndLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	fgSizer1->Add( m_MicroViaTopEndLayer, 0, wxALL, 5 );
-	
-	m_staticText10 = new wxStaticText( this, wxID_ANY, _("Micro Bottom"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_staticText10->Wrap( -1 );
-	fgSizer1->Add( m_staticText10, 0, wxALL, 5 );
-	
-	m_MicroViaBottomStartLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	fgSizer1->Add( m_MicroViaBottomStartLayer, 0, wxALL, 5 );
-	
-	m_MicroViaBottomEndLayer = new PCB_LAYER_BOX_SELECTOR( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	fgSizer1->Add( m_MicroViaBottomEndLayer, 0, wxALL, 5 );
+	bSizer2->Add( fgSizer1, 1, wxEXPAND|wxALL, 5 );
 	
 	
-	bSizes->Add( fgSizer1, 1, wxEXPAND|wxALL, 5 );
+	bSizer5->Add( bSizer2, 2, wxEXPAND, 5 );
 	
-	m_staticline1 = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
-	bSizes->Add( m_staticline1, 0, wxEXPAND | wxALL, 5 );
+	
+	bSizes->Add( bSizer5, 1, wxEXPAND, 5 );
+	
+	m_staticline2 = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
+	bSizes->Add( m_staticline2, 0, wxEXPAND | wxALL, 5 );
 	
 	m_stdButtons = new wxStdDialogButtonSizer();
 	m_stdButtonsOK = new wxButton( this, wxID_OK );
--- pcbnew/dialogs/dialog_via_layers_base.h
+++ pcbnew/dialogs/dialog_via_layers_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Feb 16 2016)
+// C++ code generated with wxFormBuilder (version Aug 23 2017)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO "NOT" EDIT THIS FILE!
@@ -15,16 +15,17 @@ class DIALOG_SHIM;
 class PCB_LAYER_BOX_SELECTOR;
 
 #include "dialog_shim.h"
-#include <wx/string.h>
-#include <wx/stattext.h>
-#include <wx/gdicmn.h>
-#include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
-#include <wx/bmpcbox.h>
-#include <wx/sizer.h>
-#include <wx/statline.h>
+#include <wx/string.h>
+#include <wx/font.h>
+#include <wx/grid.h>
+#include <wx/gdicmn.h>
 #include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/bmpcbox.h>
+#include <wx/statline.h>
 #include <wx/dialog.h>
 
 ///////////////////////////////////////////////////////////////////////////
@@ -38,22 +39,16 @@ class DIALOG_VIA_LAYERS_BASE : public DIALOG_SHIM
 	private:
 	
 	protected:
+		wxGrid* m_gridViaStack;
+		wxButton* m_buttonAddVia;
+		wxButton* m_buttonRemoveVia;
 		wxStaticText* m_staticText3;
 		wxStaticText* m_staticText71;
 		wxStaticText* m_staticText8;
-		wxStaticText* m_staticText9;
+		wxStaticText* m_staticTextViaName;
 		PCB_LAYER_BOX_SELECTOR* m_ViaStartLayer;
 		PCB_LAYER_BOX_SELECTOR* m_ViaEndLayer;
-		wxStaticText* m_staticText5;
-		PCB_LAYER_BOX_SELECTOR* m_BlindBurriedViaStartLayer;
-		PCB_LAYER_BOX_SELECTOR* m_BlindBurriedViaEndLayer;
-		wxStaticText* m_staticText7;
-		PCB_LAYER_BOX_SELECTOR* m_MicroViaTopStartLayer;
-		PCB_LAYER_BOX_SELECTOR* m_MicroViaTopEndLayer;
-		wxStaticText* m_staticText10;
-		PCB_LAYER_BOX_SELECTOR* m_MicroViaBottomStartLayer;
-		PCB_LAYER_BOX_SELECTOR* m_MicroViaBottomEndLayer;
-		wxStaticLine* m_staticline1;
+		wxStaticLine* m_staticline2;
 		wxStdDialogButtonSizer* m_stdButtons;
 		wxButton* m_stdButtonsOK;
 		wxButton* m_stdButtonsCancel;
@@ -66,7 +61,7 @@ class DIALOG_VIA_LAYERS_BASE : public DIALOG_SHIM
 	
 	public:
 		
-		DIALOG_VIA_LAYERS_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Via layer config"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 354,236 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
+		DIALOG_VIA_LAYERS_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Via layer config"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 667,334 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER ); 
 		~DIALOG_VIA_LAYERS_BASE();
 	
 };
